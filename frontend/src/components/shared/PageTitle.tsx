ef51b1fd09c109d7326d146377495dcd3443a870
dani6666 <danielwz99@gmail.com>
2023-01-07 23:44:13 +0100

    adding PageTitle component

---
+++ frontend/src/components/shared/PageTitle.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Typography, useTheme } from "@mui/material";
+
+const PageTitle = (props: any) => {
+  const theme = useTheme();
+
+  return (
+    <Typography
+        variant="h4"
+        component="h2"
+        sx={{ color: theme.palette.primary.contrastText, p: 5 }}
+      >
+        {props.titleText}
+      </Typography>
+  );
+};
+
+export default PageTitle;
