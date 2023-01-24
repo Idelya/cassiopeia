39640c37eb26f365504197531fdd118c04ae20c8
Elentija <hornik.daria@gmail.com>
2023-01-24 12:22:22 +0100

    backend scaffolding

---
+++ backend/UserServiceAPI/Data/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class UserContext : IdentityDbContext<User, IdentityRole, string>
+    {
+        public DbSet<User> Users { get; set; }
+
+        public UserContext(DbContextOptions<UserContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ backend/UserServiceAPI/UserServiceAPI/Startup.cs
@@ -0,0 +1,59 @@
+﻿using Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Data.Models;
+
+namespace UserServiceAPI
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<UserContext>(options =>
+                 options.UseNpgsql(
+                     Configuration.GetConnectionString("DefaultConnection")));
+
+            services
+               .AddIdentity<User, IdentityRole>(opt =>
+               {
+                   //opt.Lockout.AllowedForNewUsers = false;
+                   //opt.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%^&*()-._@+";
+               })
+               .AddSignInManager<SignInManager<User>>()
+               .AddEntityFrameworkStores<UserContext>()
+               .AddDefaultTokenProviders();
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors("CorsPolicy");
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            ////app.UseSwagger();
+            //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Funtest v1"));
+        }
+    }
+}
