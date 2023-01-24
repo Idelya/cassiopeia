3464da1ef027f5ee88442e7a619f68c951fac494
Elentija <hornik.daria@gmail.com>
2023-01-24 12:22:22 +0100

    backend scaffolding

---
+++ backend/UserServiceAPI/Data/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Data.Models
+{
+    public class User: IdentityUser<string>
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public bool Block { get; set; }
+        public string? ReasonOfBlocking { get; set; }
+        public string  Username { get; set; }
+    }
+}
